3bb2fc940393d6901d3845bae5e8103029e03ad4
cheng-zhao <chengzhao03@gmail.com>
2021-01-18 15:00:37 +0100

    add license info

--- python/cosmoEZ.py
+++ python/cosmoEZ.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+# cosmoEZ.py: this file is part of the pyEZmock package.
+#
+# pyEZmock: Python wrapper of Effective Zel'dovich approximation mock (EZmock).
+#
+# Github repository:
+#       https://github.com/cheng-zhao/pyEZmock
+#
+# Copyright (c) 2021 Cheng Zhao <zhaocheng03@gmail.com>
+#   
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import numpy as np
 from scipy.special import hyp2f1
 from scipy.misc import derivative
